9a969ad1c7346050f7944ec46e2b35cb5874f06b
UtkinaAM <anastas.utkina2017@yandex.ru>
2025-10-20 15:43:35 +0300

    resolve conflict in db.py

--- db.py
+++ db.py
@@ -1,10 +1,31 @@
 import sqlite3
 import random
 
-# test2
-
 
 def get_random_prediction():
     with open("predictions.txt", "r", encoding="utf-8") as f:
         all_of_them = [line.strip() for line in f if line.strip()]
     return random.choice(all_of_them)
+
+def homework_creating():
+    connection = sqlite3.connect('mydatabase.db')
+    cursor = connection.cursor()
+
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS homework(
+                   subject TEXT NOT NULL,
+                   number INTEGER,
+                   day TEXT,
+                   flag, BLOB
+                   )
+                   ''')
+    connection.commit()
+    connection.close()
+
+def homework_adding(subject, number, day, flag=0):
+    connection = sqlite3.connect('mydatabase.db')
+    cursor = connection.cursor()
+    cursor.execute('UPDATE ')
+
+
+
+
