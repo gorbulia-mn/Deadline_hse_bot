afe28020046e913eff416c00e32db2a15eb134fe
mrzhkovaa <lera.filly@gmail.com>
2025-10-21 10:03:45 -0700

    добавила напоминания и реакцию на кнопку ближайшие экзы

--- formatting.py
+++ formatting.py
@@ -2,8 +2,10 @@ import datetime as dt
 
 def d(d: dt.datetime) -> str: 
     return d.strftime("%d.%m.%Y")
+
 def t(d: dt.datetime) -> str: 
     return d.strftime("%H:%M")
+
 
 def format_exams_list(exams: list[dict]) -> str:
     if not exams:
@@ -13,8 +15,9 @@ def format_exams_list(exams: list[dict]) -> str:
         lines.append(f'• <b>{e['course']}</b> — {e['exam_type']} {d(e['at'])} {t(e['at'])}')
     return "\n".join(lines)
 
+
 def format_exam_reminder(course: str, exam_type: str, at: dt.datetime, time_left: str) -> str:
-    return f"Напоминание\nДо {exam_type} по <b>{course}</b> осталось {time_left}.\nДата: {d(at)}\n⏰ Время: <b>{t(at)}</b>"
+    return f"Напоминание\nДо {exam_type} по <b>{course}</b> осталось {time_left}.\nДата: {d(at)}\n Время: <b>{t(at)}</b>"
 
 
 def split_text_hw(text: str) -> tuple[str, str, int, dt.datetime]:
@@ -23,3 +26,10 @@ def split_text_hw(text: str) -> tuple[str, str, int, dt.datetime]:
     date_time_together = f"{d_str} {t_str}"
     date_time = dt.datetime.strptime(date_time_together, "%d.%m.%y %H:%M")
     return sub, t, n, date_time
+
+
+def split_text_exam(text: str) -> tuple[str, str, dt.datetime]:
+    # формат: "<курс> <тип_экза> <дд.мм.гггг> <чч:мм>"
+    course, exam_type, d_str, t_str = text.strip().split()
+    at = dt.datetime.strptime(f"{d_str} {t_str}", "%d.%m.%Y %H:%M")
+    return course, exam_type, at
